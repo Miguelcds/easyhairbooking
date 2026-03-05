98e63bbb1151922338471bacdc06f119bf645724
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-05 01:45:11 +0100

    refactor: improve user model schema formatting and comments

--- Backend/src/api/models/user.model.js
+++ Backend/src/api/models/user.model.js
@@ -5,8 +5,8 @@ const { use } = require("react");
 
 /* 
 * Type:
-*
-*
+* Trim:
+* Require:
 *
 *
 *
@@ -15,14 +15,19 @@ const { use } = require("react");
 
 */
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, trim: true, require: true, minLength: 5 },
-  email: { type: String, trim: true, require: true, unique: true },
-  password: {
-    type: String,
-    trim: true,
-    require:true,
-    minLength: [8, "La contraseña tiene que tener al menos 8 caracteres"]
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true, require: true, minLength: 5 },
+    email: { type: String, trim: true, require: true, unique: true },
+    password: {
+      type: String,
+      trim: true,
+      require: true,
+      minLength: [8, "La contraseña tiene que tener al menos 8 caracteres"],
+    },
+    role: { type: String, enum: ["admin", "user"], default: "user" }, // // Para Facilitar esta parte: El primer admin se insertará manualmente modificando el documento directamente en MongoAtlas. Podrá tener el rol “admin”
   },
-  role:{type:String, enum:["admin", "user"], default:"user"} // // Para Facilitar esta parte: El primer admin se insertará manualmente modificando el documento directamente en MongoAtlas. Podrá tener el rol “admin”
-});
+  {
+    timestamps: true,
+  },
+);
