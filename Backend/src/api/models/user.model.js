46487a226313ab720d661b688e3f41a9ce8f1ffb
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-05 01:41:39 +0100

    feat: add models for appointment, employee, slot, and user

---
+++ Backend/src/api/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcrypt"); // Libreria Encriptacion
+const { use } = require("react");
+
+/* 
+* Type:
+*
+*
+*
+*
+*
+
+
+
+*/
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, trim: true, require: true, minLength: 5 },
+  email: { type: String, trim: true, require: true, unique: true },
+  password: {
+    type: String,
+    trim: true,
+    require:true,
+    minLength: [8, "La contraseña tiene que tener al menos 8 caracteres"]
+  },
+  role:{type:String, enum:["admin", "user"], default:"user"} // // Para Facilitar esta parte: El primer admin se insertará manualmente modificando el documento directamente en MongoAtlas. Podrá tener el rol “admin”
+});
