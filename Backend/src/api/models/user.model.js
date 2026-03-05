22a01d5fc9f5d46ae4f07eac27c7794847ae5a4e
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-05 13:26:34 +0100

    refactor: fix user model schema issues and improve readability

--- Backend/src/api/models/user.model.js
+++ Backend/src/api/models/user.model.js
@@ -1,33 +1,41 @@
 const mongoose = require("mongoose");
-
 const bcrypt = require("bcrypt"); // Libreria Encriptacion
-const { use } = require("react");
 
 /* 
 * Type:
 * Trim:
 * Require:
+* minLength:
+* 
 *
-*
-*
-
-
-
 */
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, trim: true, require: true, minLength: 5 },
-    email: { type: String, trim: true, require: true, unique: true },
+    name: { type: String, trim: true, required: true, minlength: 5 },
+    email: { type: String, trim: true, required: true, unique: true },
     password: {
       type: String,
       trim: true,
-      require: true,
-      minLength: [8, "La contraseña tiene que tener al menos 8 caracteres"],
+      required: true,
+      minlength: [8, "La contraseña tiene que tener al menos 8 caracteres"],
     },
-    role: { type: String, enum: ["admin", "user"], default: "user" }, // // Para Facilitar esta parte: El primer admin se insertará manualmente modificando el documento directamente en MongoAtlas. Podrá tener el rol “admin”
+    profileImgUrl: {type:String , default:"" }, // Pondre Cuando Este Con la Parte Claudinary
+    profileImgId:{type:String, trim:true},
+    role: { type: String, enum: ["admin", "client"], default: "client" }, // // Para Facilitar esta parte: El primer admin se insertará manualmente modificando el documento directamente en MongoAtlas. Podrá tener el rol “admin”
   },
   {
     timestamps: true,
   },
 );
+
+
+// Funcion Para hashear Contraseña, pendiente Cuando toque
+
+// userSchema.pre()
+
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
