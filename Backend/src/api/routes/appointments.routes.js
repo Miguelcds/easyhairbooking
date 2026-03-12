90d00ef723ad17902d66bc183a78c7efeabdd448
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-12 22:31:32 +0100

    feat: add routes to retrieve client and admin appointments

--- Backend/src/api/routes/appointments.routes.js
+++ Backend/src/api/routes/appointments.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {bookAppointment, changeAppointment} = require('../controllers/appointment.controller');
+const {bookAppointment, changeAppointment, getAppointmentsAdmin, getAppointmentsClient} = require('../controllers/appointment.controller');
 
 const {isAuth} = require("../../middlewares/auth.middleware");
 
@@ -10,6 +10,12 @@ const appointmentRouter = express.Router();
 appointmentRouter.post("/",isAuth(["client", "admin"]),bookAppointment)
 
 appointmentRouter.patch("/:id", isAuth(["client", "admin"]), changeAppointment)
+
+appointmentRouter.get("/my",isAuth(["client", "admin"]),getAppointmentsClient)
+
+appointmentRouter.get("/",isAuth(["admin"]),getAppointmentsAdmin)
+
+
 
 module.exports = appointmentRouter
 
