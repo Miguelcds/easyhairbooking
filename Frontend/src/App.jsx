091309853847f9e569f4ce9280fcdafbd4787a93
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-16 20:20:12 +0100

    feat: implement PrivateRoute component for protected routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,36 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
-import Admin from './pages/Admin.jsx'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Admin from "./pages/Admin.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 function App() {
-  
   return (
-   <Routes>
-      <Route path='/' element={<Home/> }/>
-      <Route path='/dashboard' element={<Dashboard/> }/>
-      <Route path='/login' element={<Login/> }/>
-      <Route path='/register' element={<Register/> }/>
-      <Route path='/admin' element={<Admin/> }/>
-   </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/admin"
+        element={
+          <PrivateRoute>
+            <Admin />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
