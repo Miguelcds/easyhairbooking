441e1923d218a8d12e608e8d3c830bcfee1cdbb2
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-03-18 17:56:01 +0100

    feat: add getEmployeesService to fetch employee data from API

---
+++ Frontend/src/services/employee.service.js
@@ -0,0 +1,32 @@
+import axiosClient from "./axios.config";
+
+/* 
+
+Recuerda! La Respuesta de Axios es:
+
+result = {
+  data: [...],        // ← la respuesta real del servidor
+  status: 200,
+  headers: {...},
+  config: {...}
+}
+
+
+Por eso cogemos result.data 
+
+*/
+
+
+export const getEmployeesService = async () => {
+
+    try {
+       const result = await axiosClient.get("/employee")
+       return result.data
+
+    } catch (error) {
+        console.error(error);  
+    }
+
+}
+
+
